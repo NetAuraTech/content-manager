32281298aaef9725ebaee3740890e864e211c377
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-08-07 22:38:10 +0200

    fix(routes)

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Netauratech\ContentManager\Http\Controllers\PageController;
+use Netauratech\CoreCms\Contracts\ContentProviderInterface;
 
 /**
  * Pages
  */
-Route::get('/', [PageController::class, 'homepage'])->name('homepage');
-Route::get('/{slug}', [PageController::class, 'show'])->name('page.show');
+Route::get('/', [PageController::class, 'homepage'])->name('home');
+Route::fallback(function (ContentProviderInterface $contentProvider) {
+    $slug = request()->path();
+
+    $page = $contentProvider->getContentBySlug($slug);
+
+    if (!$page || $page->type !== 'page' || $page->status !== 'published') {
+        abort(404, 'Page introuvable ou non publiée.');
+    }
+
+    return view('content-manager::front.page', [
+        'page' => $page,
+        'isHomepage' => false,
+    ]);
+})->name('page.show');
