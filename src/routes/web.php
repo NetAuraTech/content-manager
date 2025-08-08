f06186d96cc046f95da91d3f599c0647a6f51204
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-08-08 15:24:38 +0200

    feat(seo)
    - Sitemap & robots.txt

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Netauratech\ContentManager\Http\Controllers\PageController;
+use Netauratech\ContentManager\Http\Controllers\SeoContentController;
 use Netauratech\CoreCms\Contracts\ContentProviderInterface;
 
 /**
  * Pages
  */
 Route::get('/', [PageController::class, 'homepage'])->name('home');
+Route::get('/sitemap.xml', [SeoContentController::class, 'sitemap'])->name('sitemap');
+Route::get('/robots.txt', [SeoContentController::class, 'robotsTxt'])->name('robots.txt');
+
 Route::fallback(function (ContentProviderInterface $contentProvider) {
     $slug = request()->path();
 
