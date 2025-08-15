572ae5b767902fc3d3e6a75b50c44fbcd8d91bab
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-08-16 00:33:20 +0200

    fix(views)
    - Remove $menu variable

--- src/resources/views/shared/blocks/links.blade.php
+++ src/resources/views/shared/blocks/links.blade.php
@@ -42,7 +42,7 @@
                         }
 
                     @endphp
-                    <li><a href="{{ $path }}" {{ menu_active($menu, $path) }}>{{ $label }}</a></li>
+                    <li><a href="{{ $path }}" {{ menu_active($path) }}>{{ $label }}</a></li>
                 @endforeach
             @endif
         </ul>
