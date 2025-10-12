757f369a16d6d962a6e2dab23b09fdc1b8b2e3da
Lhomme Aymeric <aymericlhomme@orange.fr>
2025-10-13 01:37:21 +0200

    feat(ts)
    - Add lightbox & automatic-gallery

--- src/resources/ts/app.ts
+++ src/resources/ts/app.ts
@@ -1,5 +1,12 @@
 import {InputChoicesElement, SelectChoicesElement} from "./elements/Choices";
 
+import {clearAllBodyScrollLocks} from "body-scroll-lock";
+import Lightbox from "./elements/Lightbox";
+import AutomaticGallery from "./elements/AutomaticGallery";
+
+Lightbox.defineElement();
+AutomaticGallery.defineElement();
+
 customElements.define('input-choices', InputChoicesElement, {
     extends: 'input',
 });
@@ -7,3 +14,5 @@ customElements.define('input-choices', InputChoicesElement, {
 customElements.define('select-choices', SelectChoicesElement, {
     extends: 'select',
 });
+
+clearAllBodyScrollLocks();
